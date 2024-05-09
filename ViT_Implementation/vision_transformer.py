15e82ea155e20e34e0317c8eac0ce2661a9f90aa
average-avancha <ritvikreddy2001@gmail.com>
2024-05-09 15:53:33 -0500

    cleaning up implementation

--- ViT_Implementation/vision_transformer.py
+++ ViT_Implementation/vision_transformer.py
@@ -1,17 +1,3 @@
-import torch
-import torch.nn as nn
-from torch.optim import Adam
-from torch.nn import CrossEntropyLoss
-from torch.utils.data import DataLoader
-
-from torchvision.transforms import ToTensor
-from torchvision.datasets.mnist import MNIST
-
-import numpy as np
-
-np.random.seed(0)
-torch.manual_seed(0)
-
 # Reference code:
 # https://medium.com/@brianpulfer/vision-transformers-from-scratch-pytorch-a-step-by-step-guide-96c3313c2e0c
 
